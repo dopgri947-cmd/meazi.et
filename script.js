fcdbf7a3b979b9b03e452647d9e03c7817f36958
dopgri947-cmd <dopgri947@gmail.com>
2026-06-19 14:05:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const botToken = "YOUR_BOT_TOKEN";
+const chatId = "YOUR_CHAT_ID";
 
+function order(item) {
+  fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: "New Order: " + item
+    })
+  });
+
+  alert("Order sent!");
+}
