449858bc8dd32a742bda9d776d5e5c5cc3c4f5c1
dopgri947-cmd <dopgri947@gmail.com>
2026-06-20 19:47:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-const botToken = "YOUR_BOT_TOKEN";
-const chatId = "YOUR_CHAT_ID";
-
-function order(item) {
-  fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({
-      chat_id: chatId,
-      text: "New Order: " + item
-    })
-  });
-
-  alert("Order sent!");
-}
+console.log("MEAZI DRINK SHOP Loaded Successfully!");
